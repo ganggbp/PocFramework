64ee5d1aba494d638aa0648a9ff64b8f0f647cb3
Burit Boonkorn <burit_bk@hotmail.com>
2024-06-12 23:48:34 +0700

    Initial Commit

---
+++ PocFramework/PocFramework.h
@@ -0,0 +1,18 @@
+//
+//  PocFramework.h
+//  PocFramework
+//
+//  Created by Burit Boonkorn on 12/6/2567 BE.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PocFramework.
+FOUNDATION_EXPORT double PocFrameworkVersionNumber;
+
+//! Project version string for PocFramework.
+FOUNDATION_EXPORT const unsigned char PocFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PocFramework/PublicHeader.h>
+
+
